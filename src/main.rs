4a1124cdbee266d55d0b24701d0108516e73ae52
Brahian E. Soto Mercedes <brahian@somebrel.com>
2017-07-08 02:24:35 +0200

    Reads a .csv file and prints each record to stdout.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+extern crate csv;
+
+use std::env;
+use std::error::Error;
+use std::ffi::OsString;
+use std::process;
+
+// This introduces a type alias so that we can conveniently reference
+// our record type.
+type Record = (String, String, String, String);
+
+fn run() -> Result<(), Box<Error>> {
+    let file_path = get_first_arg()?;
+    let mut rdr = csv::Reader::from_path(file_path)?;
+    for result in rdr.deserialize() {
+        let record: Record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+}
+
+/// Returns the first positional argument sent to this process. If
+/// there are no arguments, then this returns an error.
+fn get_first_arg() -> Result<OsString, Box<Error>> {
+    match env::args_os().nth(1) {
+        None => Err(From::from("expected 1 argument, but got none")),
+        Some(file_path) => Ok(file_path),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = run() {
+        println!("{}", err);
+        process::exit(1);
+    }
 }
